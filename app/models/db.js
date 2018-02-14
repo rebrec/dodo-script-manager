8e3004e5739eff37cfb4bdc3aa73eed3b712224d
François Laplaud <elfrancesco+github@gmail.com>
2018-02-14 09:01:45 +0100

    added database auto compact

--- app/models/db.js
+++ app/models/db.js
@@ -17,7 +17,9 @@ function loadAllDatabases() {
 module.exports = function (config) {
 
     db.scriptData = new Datastore({filename: config.dbScriptDataFilename});
+    db.scriptData.persistence.setAutocompactionInterval(5 * 60 * 1000);
     db.settings = new Datastore({filename: config.dbSettingsFilename});
+    db.settings.persistence.setAutocompactionInterval(5 * 60 * 1000);
     Promise.promisifyAll(db.scriptData);
     Promise.promisifyAll(db.settings);
     return {
