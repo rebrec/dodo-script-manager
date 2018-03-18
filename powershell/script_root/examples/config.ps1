6fc5e5cfdb75de036e81720c1d8bb149080c2718
François Laplaud <elfrancesco+github@gmail.com>
2018-03-19 00:31:45 +0100

    typo correction

--- powershell/script_root/examples/config.ps1
+++ powershell/script_root/examples/config.ps1
@@ -1,10 +1,10 @@
 ﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 # Change thoses values as wished
-$global:DODO_HOSTNAME = "localhost
+$global:DODO_HOSTNAME = "localhost"
 $global:DODO_PORT = 8088
 
-$global:DEFAULT_DODO_BASE_DIR     = "$scriptPath" 
+$global:DEFAULT_DODO_BASE_DIR     = "$scriptPath"
 #$global:DEFAULT_DODO_BIN_DIR 	  = "some specific path ?"
 $global:DEFAULT_DODO_LIB_PATH     = "$scriptPath\lib\dodoLib.ps1"
 #$global:DEFAULT_SpecificScript    = 'The\full\path\to\a\dodo\script\you\want\to\test'
