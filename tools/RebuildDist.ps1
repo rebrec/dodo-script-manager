b72321c190c9e40c7d2b776e817f2d92fa37c559
François Laplaud <elfrancesco+github@gmail.com>
2018-03-21 15:44:56 +0100

    added Setup and build script

---
+++ tools/RebuildDist.ps1
@@ -0,0 +1,85 @@
+﻿param (
+	[string] $build="prod",
+    [string] $RemotePath="d:\tmp\netlogon\"
+)
+if (($build -ine "dev") -and ($build -ine "prod")){
+    throw "-Build must be folowed by keyword 'dev' or 'build'"
+}
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$projectRoot   = $(Get-Item $scriptPath).parent.FullName
+$distFolder    = "$projectRoot\dist"
+$configFolder  = "$projectRoot\config"
+$configFile    = "$configFolder\config-$build.ps1"
+$dodoFolder    = "$distFolder\DODO"
+$scriptRoot    = "$projectRoot\powershell\script_root"
+$exampleFolder = "$scriptRoot\examples"
+
+Write-Host @"
+
+This script update the dist folder to reflect the latest modifications you
+may have done within $projectRoot
+projectRoot     = $projectRoot
+distFolder      = $distFolder
+configFolder    = $configFolder
+configFile      = $configFile
+dodoFolder      = $dodoFolder
+scriptRoot      = $scriptRoot
+exampleFolder   = $exampleFolder
+
+"@
+
+if ((Test-Path $distFolder) -ne $true) {
+    Write-Warning "$distFolder doesn't exist ! Please run first SetupDist.ps1 !"
+    
+} else {
+    Remove-Item "$distFolder" -Recurse -Force | out-null
+}
+if ((Test-Path "$configFolder") -ne $true) { 
+    Write-Warning "$configFolder doesn't exist ! Please run first SetupDist.ps1 !"
+}
+
+Write-Host "[+] Creation of dist folder ($distFolder)"
+New-Item $distFolder -ItemType directory | out-null
+
+Write-Host "[+] Copy of the folder structure"
+Copy-Item  $scriptRoot -Destination $distFolder -Recurse | out-null
+Rename-Item -Path "$distFolder\script_root\" -NewName "DODO" | out-null
+
+Write-Host "[+] Removing unuseful stuff"
+Remove-Item -Recurse -Force "$dodoFolder\examples"| out-null
+Remove-Item -Recurse -Force "$dodoFolder\src" | out-null
+
+Copy-Item -path "$configFile" -Destination "$dodoFolder\config.ps1" -Force | out-null
+
+
+
+
+if ($build -ieq "prod") {
+    if ($RemotePath -eq ""){
+    Write-Warning @"
+You will now have to copy manually $distFolder to your Netlogon directory.
+Please note that you can automatically copy this folder to any location (your Netlogon directory
+for instance) by using 
+    
+    RebuildDist.ps1 -RemotePath "\\YourDomain.corp\Netlogon"
+
+This would copy files to \\YourDomain.corp\Netlogon\Dodo
+"@
+    } else {
+        Write-Host "[+] Copying files to $RemotePath"
+        if (Test-Path "$RemotePath"){
+            Copy-Item "$dodoFolder" -Destination "$RemotePath" -Recurse -Force
+        } else {
+            Write-Warning "$RemotePath doesn't exists ! Skipping..."
+            }
+    }
+}
+
+
+Write-Host @"
+################################################################################################
+Upgrade Done ! Build "$build"
+################################################################################################
+"@
--- tools/SetupDist.ps1
+++ tools/SetupDist.ps1
@@ -1,34 +1,87 @@
-﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+﻿param(
+    [switch] $Force
+)
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-$projectRoot = $(Get-Item $scriptPath).parent.parent.FullName
+$projectRoot = $(Get-Item $scriptPath).parent.FullName
 $distFolder = "$projectRoot\dist"
+$dodoFolder = "$distFolder\DODO"
 $scriptRoot = "$projectRoot\powershell\script_root"
 $exampleFolder = "$scriptRoot\examples"
+Write-Host @"
+projectRoot     = $projectRoot
+distFolder      = $distFolder
+dodoFolder      = $dodoFolder
+scriptRoot      = $scriptRoot
+exampleFolder   = $exampleFolder
+"@
 
+# Remove DIST FOLDER
 if (Test-Path $distFolder) {
-    Write-Host "Removing existing $distFolder"
-    Remove-Item "$distFolder" -Recurse -Force | out-null
+    if ($Force){
+        Write-Host "Removing existing $distFolder"
+        Remove-Item "$distFolder" -Recurse -Force | out-null
+    } else {
+        Write-Warning @"
+This script has already been executed !
+
+This will then :
+- Remove dist folder
+- Remove config folder (please make backups if needed)
+- Remove script folder (which may contain your personal scripts !)
+
+It is STRONGLY adviced to make a copy of :
+- $distFolder
+- $scriptRoot\scripts
+- $projectRoot\config
+
+If you are SURE to want to start from scratch all over again, use the -Force parameter (after doing backups !).
+"@
+        throw "SetupDist has already been executed ! If you are sure to want to start all over again use the -Force parameter"
+    }
 }
 
-Write-Host "Creation of dist folder ($distFolder)"
-New-Item $distFolder -ItemType directory | out-null
+# Copy of Script folder example structure
+if (Test-Path $scriptRoot\scripts) {
+    Write-Host "[+] Removing $scriptRoot\scripts"
+    Remove-Item -Recurse -force "$scriptRoot\scripts"
+}
 
-Write-Host "Copy of the folder structure"
+Write-Host "[+] Creation of script folder ($scriptRoot\scripts)"
+Write-Host "[+] Copy of example scripts"
+Copy-Item  "$scriptRoot\examples\scripts" -Destination $scriptRoot -Recurse | out-null   
+
+
+New-Item -ItemType directory "$distFolder" | out-null
 Copy-Item  $scriptRoot -Destination $distFolder -Recurse | out-null
 Rename-Item -Path "$distFolder\script_root\" -NewName "DODO" | out-null
-Copy-Item "$exampleFolder\config.ps1" "$distFolder\DODO" -Force | out-null
 
-Write-Host "Removing unuseful stuff"
-Remove-Item -Recurse -Force "$distFolder\DODO\examples"| out-null
-Remove-Item -Recurse -Force "$distFolder\DODO\src" | out-null
+Write-Host "[+] Removing unuseful stuff"
+Remove-Item -Recurse -Force "$dodoFolder\examples"| out-null
+Remove-Item -Recurse -Force "$dodoFolder\src" | out-null
+
+
+if (Test-Path "$projectRoot\config") { 
+    Write-Host "[+] Removing $projectRoot\config"
+    Remove-Item -Recurse -force "$projectRoot\config"
+}
+
+Write-Host "[+] Copy of config folder"
+New-Item "$projectRoot\config" -ItemType directory -Force | out-null
+Write-Host "[+] Creating $projectRoot\config"
+Copy-Item -path "$exampleFolder\config.ps1" -Destination "$projectRoot\config\config-prod.ps1" -Force | out-null
+Copy-Item -path "$exampleFolder\config.ps1" -Destination "$projectRoot\config\config-dev.ps1" -Force | out-null
+Write-Warning @"
+
+Please edit at least the variable global:DODO_HOSTNAME in :
+- $dodoFolder\config\config-prod.ps1 (MUST be filled)
+- $dodoFolder\config\config-dev.ps1 (if you plan to have a dev platform)
+
+Then you can run RebuildDist.ps1
+"@
 
 Write-Host @"
 ################################################################################################
 # Setup Done !
-################################################################################################
-# A few things need to be done manually : 
-# - edit $distFolder\DODO\config.ps1
-#     - change the variable global:DODO_HOSTNAME = "YOUR_DODO_SERVER_IP_HERE"
-# - copy $distFolder\DODO to your company's Netlogon folder
 ################################################################################################
 "@
